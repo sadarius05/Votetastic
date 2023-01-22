72620c7f5b65003ccd0b05e0740c151f2800ea01
Souliman <souleymane.sada@gmail.com>
2023-01-22 11:06:28 +0000

    WIP signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -21,10 +27,24 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  login() {
+  async login() {
     console.log(this.form.value);
+    const { data, error } = await this.auth.login(this.form.value);
+    console.log(data);
+    if (error) {
+    } else {
+      this.router.navigateByUrl('app', { replaceUrl: true });
+    }
   }
-  register() {
-    throw new Error('Method not implemented.');
+  async register() {
+    console.log(this.form.value);
+    const { data, error } = await this.auth.createAccount(this.form.value);
+    console.log(data);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      this.router.navigateByUrl('app', { replaceUrl: true });
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://visfyudosytneysuehci.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpc2Z5dWRvc3l0bmV5c3VlaGNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQzNjI2NDksImV4cCI6MTk4OTkzODY0OX0.EXsuOq-6wu3g9IpJOR19rqVvgj73dIflhFxgKysPjXI',
 };
 
 /*
