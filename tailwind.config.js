94b27becb957ecdf5a8e7ed4a7e80986cac9ea2c
Souliman <souleymane.sada@gmail.com>
2023-01-22 01:51:55 +0000

    Telwind and Login() Work In Progress

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
