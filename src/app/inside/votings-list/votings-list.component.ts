1ce49477189caf0afe92285b00ba02e7d9557dbf
Souliman <souleymane.sada@gmail.com>
2023-01-24 00:09:25 +0000

    WIP add votings.

--- src/app/inside/votings-list/votings-list.component.ts
+++ src/app/inside/votings-list/votings-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-votings-list',
   templateUrl: './votings-list.component.html',
-  styleUrls: ['./votings-list.component.scss']
+  styleUrls: ['./votings-list.component.scss'],
 })
 export class VotingsListComponent implements OnInit {
-
-  constructor() { }
+  votings: any[] = [];
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.loadVotings();
   }
 
+  async loadVotings() {
+    this.votings = await this.dataService.getVotings();
+    console.log(this.votings);
+  }
+
+  async startVoting() {
+    await this.dataService.startVoting();
+  }
 }
